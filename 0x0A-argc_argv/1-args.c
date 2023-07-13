6fe5c1eb0cb0cbcd258fc8d0f4a06b18326b1c90
salma <salmahelaltur@gmail.com>
2023-07-12 17:19:12 -0700

    file_1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - asd
+ * @argc: ass
+ * @argv: sas
+ * Return: num
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
