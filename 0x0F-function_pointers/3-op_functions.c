99ab4c8ec30dcd811cab764b26ae22f24d7c90c7
salma <salmahelaltur@gmail.com>
2023-07-19 17:55:45 -0700

    file_0

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - qw
+ * @a: num1
+ * @b: num2
+ * Return: nn
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - 	qw
+ * @a: vp1
+ * @b: vp2
+ * Return: asd
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - qwed
+ * @a: vp2
+ * @b: vp1
+ * Return: nn
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - vcx
+ * @a: vp1
+ * @b: vp2
+ * Return: nn
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - zxcv
+ * @a: vp1
+ * @b: vp2
+ * Return: nn
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
