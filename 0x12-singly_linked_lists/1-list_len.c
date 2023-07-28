939db65f6563215670684ac9a5228081f4486523
salma <salmahelaltur@gmail.com>
2023-07-27 18:54:51 -0700

    dds

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - func1
+ * @h: vp1
+ * Return: num
+ */
+size_t list_len(const list_t *h)
+{
+	size_t sa = 0;
+
+	while (h)
+	{
+		sa++;
+		h = h->next;
+	}
+	return (sa);
+}                                                             
