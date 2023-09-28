5e0fabeca4f4a02df6ea2e6d58415c8ed044578e
salma <salmahelaltur@gmail.com>
2023-09-28 10:35:14 -0700

    dfef

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - dff;cekln
+ * @head: vp1
+ **/
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+	return;
+
+	while (head->next)
+	{
+	head = head->next;
+	free(head->prev);
+	}
+	free(head);
+}
